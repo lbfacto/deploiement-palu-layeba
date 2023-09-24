4949d8f1617af3f523e5cf1e76286655d5fcf861
lbfacto <48971071+lbfacto@users.noreply.github.com>
2023-09-24 19:23:37 +0000

    11

--- AutoML.py
+++ AutoML.py
@@ -38,7 +38,7 @@ import matplotlib.pyplot as plt
 
 st.set_page_config(layout="wide", page_title=" 💻📊 Analyse de Données UVS ")
 st.markdown('<style>' + open('./style/style.css').read() + '</style>', unsafe_allow_html=True)
-git, linkedin = st.columns(2)
+# git, linkedin = st.columns(2)
 # git.markdown("[![Foo](https://img.icons8.com/material-outlined/48/000000/github.png)](https://github.com/lbfacto/A_BA_UVS_ProjetPaludisme)")
 git.info(" 👆 Récupérez l'intégralité du code ici")
 
